20a836ea1eaa5005847363ffa3a058256fe3780d
feizmoalla <fmoalla12@gmail.com>
2021-10-25 23:54:56 +0100

    45min

--- src/main/java/io/getarrays/server/ServerApplication.java
+++ src/main/java/io/getarrays/server/ServerApplication.java
@@ -1,13 +1,29 @@
 package io.getarrays.server;
 
+import io.getarrays.server.enumeration.Status;
+import io.getarrays.server.model.Server;
+import io.getarrays.server.repo.ServerRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(ServerRepo serverRepo) {
+		return args -> {
+			serverRepo.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16 GB", "Personal PC", "http://localhost:8080/server/image/server1.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.161", "Ubuntu Linux", "32 GB", "Personal PC", "http://localhost:8080/server/image/server3.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.162", "Ubuntu Linux", "64 GB", "Personal PC", "http://localhost:8080/server/image/server4.png", Status.SERVER_UP));
+			serverRepo.save(new Server(null, "192.168.1.163", "Ubuntu Linux", "8 GB", "Personal PC", "http://localhost:8080/server/image/server2.png", Status.SERVER_UP));
+
+		};
 	}
 
 }
